a06782157e9d631e5d46f2ee6e4862425008eaae
koya <sereronnfox@gmail.com>
2018-04-19 12:43:18 +0000

    kadai-tasklist

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,62 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+  
+  
+  def create
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "タスクが正常に生成されました"
+      redirect_to @task
+    else
+      flash.now[:danger] = "タスクが正常に生成されませんでした"
+      render :new
+    end
+  end
+  
+  
+  def new
+    @task = Task.new
+  end
+  
+  
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  
+  def update
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "正常更新"
+      redirect_to @task
+    else
+      flash.now[:danger] = "更新失敗"
+      render :edit
+    end
+  end
+  
+  
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    
+    flash[:success] = "正常に削除されました　"
+    redirect_to tasks_url
+  end
+  
+  private
+  
+  # Strong Params
+  def task_params
+    params.require(:task).permit(:content)
+  end
+end
