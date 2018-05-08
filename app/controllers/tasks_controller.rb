7ba132ed42b19c5733af30b12e4c19b18518812a
koya <sereronnfox@gmail.com>
2018-05-08 09:44:39 +0000

    bag fix

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
+  before_action :correct_user, only: [:create, :edit, :show, :update, :destroy]
   
   def index
     @tasks = Task.all
@@ -62,5 +63,12 @@ class TasksController < ApplicationController
   # Strong Params
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
